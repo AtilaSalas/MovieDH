4b92759a9a58a903830e0fe5f5f98d6f58f12caf
AtilaSalas <56383579+AtilaSalas@users.noreply.github.com>
2020-03-26 09:09:22 -0300

    final 1.5.7.6

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,22 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-      $random = rand(1,21);
-      $random1 = rand(1,21);
-      $random2 = rand(1,21);
-      $random3 = rand(1,21);
-      $random4 = rand(1,21);
-      $randomFive = Movie::where("id", ">", 0)
-                           ->orderBy("title")
-                           ->get();
-      $theLastFive = Movie::where("id", ">", 0)
-                           ->orderBy("release_date", "DESC")
-                           ->limit(5)
-                           ->get();
-                        // $chunk = $randomFive->chunk(5);
-                        //      dd($chunk[0]);
-
-      $vac = compact('randomFive', 'theLastFive');
-      return view("home", $vac);
+      return redirect("/");
     }
 }
